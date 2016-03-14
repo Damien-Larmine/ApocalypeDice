a467b6b929848d5fbd5cf53bb8d3bd6fb1e7456b
Deckao <Deckao@users.noreply.github.com>
2016-03-14 20:45:04 +0100

    Changing numerote_noeud

--- Arbre.ml
+++ Arbre.ml
@@ -3,7 +3,7 @@ struct
   type 'a t = Node of 'a * 'a t list
   let rec rec_num_node arbre c = match arbre with
     |Node(x,[]) -> Node((x,c),[])
-    |Node(x,h::t) -> Node((x,c),((rec_num_node h c+1)::t))
+    |Node(x,l) -> Node((x,c), (rec_num_node l c+1))
 
   let numerote_noeud arbre = rec_num_node arbre 0
 
